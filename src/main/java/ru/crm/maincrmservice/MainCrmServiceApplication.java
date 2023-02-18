2d33b29eea756ab0daabc1641437a85013f561a9
Karpeikin <mkarpeykin@phoenixit.ru>
2023-02-19 01:38:18 +0300

    First init commit

---
+++ src/main/java/ru/crm/maincrmservice/MainCrmServiceApplication.java
@@ -0,0 +1,13 @@
+package ru.crm.maincrmservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MainCrmServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MainCrmServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/crm/maincrmservice/MainCrmServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.crm.maincrmservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MainCrmServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
