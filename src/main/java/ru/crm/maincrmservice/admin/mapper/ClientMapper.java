a5b976b72b192e5b061a19deac069af51bc5c0a1
Karpeikin <mkarpeykin@phoenixit.ru>
2023-02-20 08:26:24 +0300

    Добавил получения пользователя и списка пользователей

--- src/main/java/ru/crm/maincrmservice/admin/mapper/ClientMapper.java
+++ src/main/java/ru/crm/maincrmservice/admin/mapper/ClientMapper.java
@@ -1,9 +1,14 @@
 package ru.crm.maincrmservice.admin.mapper;
 
+import lombok.val;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import ru.crm.maincrmservice.entity.client.Client;
 import ru.crm.rest.admin.openapi.model.ClientInfo;
+import ru.crm.rest.admin.openapi.model.ClientInfoPageable;
+import ru.crm.rest.admin.openapi.model.PageParams;
 
 import java.time.Instant;
 
@@ -18,6 +23,38 @@ public interface ClientMapper {
      * @param src ДТО
      * @return Сущность
      */
-    @Mapping(target = "createDate",  expression = "java(Instant.now())")
+    @Mapping(target = "createDate", expression = "java(Instant.now())")
     Client createClientEntity(ClientInfo src);
+
+    /**
+     * Создание ДТО из сущности
+     *
+     * @param src Сущность
+     * @return ДТО
+     */
+    ClientInfo createClientInfoDto(Client src);
+
+    /**
+     * Мапинг в ДТО списка клиентов с пагинацие
+     *
+     * @param clientPage  Список клиентов
+     * @param pageRequest Параметры пагинации
+     * @return ДТО с пагинацией
+     */
+    default ClientInfoPageable createClientInfoPageable(Page<Client> clientPage, PageRequest pageRequest) {
+        val clientDtoList = clientPage.stream()
+                .map(this::createClientInfoDto)
+                .toList();
+
+        PageParams pageParams = new PageParams();
+        pageParams.setPageSize(pageRequest.getPageSize());
+        pageParams.setPageCurrent(pageRequest.getPageNumber());
+        pageParams.setPageTotal(clientPage.getTotalPages());
+
+        ClientInfoPageable clientInfoPageable = new ClientInfoPageable();
+        clientInfoPageable.setData(clientDtoList);
+        clientInfoPageable.setPageParams(pageParams);
+
+        return clientInfoPageable;
+    }
 }
--- src/main/java/ru/crm/maincrmservice/admin/service/ClientService.java
+++ src/main/java/ru/crm/maincrmservice/admin/service/ClientService.java
@@ -3,14 +3,21 @@ package ru.crm.maincrmservice.admin.service;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import lombok.val;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import ru.crm.maincrmservice.admin.mapper.ClientMapper;
+import ru.crm.maincrmservice.config.AppConfig;
 import ru.crm.maincrmservice.entity.client.Client;
 import ru.crm.maincrmservice.exception.ExceptionBusyMemberShipNumber;
+import ru.crm.maincrmservice.exception.NotFoundException;
 import ru.crm.maincrmservice.helpers.ClientHelper;
 import ru.crm.maincrmservice.repository.ClientRepository;
 import ru.crm.rest.admin.openapi.model.ClientInfo;
+import ru.crm.rest.admin.openapi.model.ClientInfoPageable;
+import ru.crm.rest.admin.openapi.model.PageParams;
+
+import java.util.Optional;
 
 @Slf4j
 @Service
@@ -20,6 +27,7 @@ public class ClientService {
     private final ClientRepository clientRepository;
     private final ClientHelper clientHelper;
     private final ClientMapper clientMapper;
+    private final AppConfig appConfig;
 
     /**
      * Добавление нового клиента
@@ -36,5 +44,46 @@ public class ClientService {
         val clientEntity = clientMapper.createClientEntity(clientInfo);
 
         return clientRepository.save(clientEntity);
+    }
+
+    /**
+     * Получения клиента по номер абонимента
+     *
+     * @param memberShipNumber Номер аббонимента
+     * @return Клиент
+     */
+    @Transactional
+    public Client getClientByMemberShipNumber(Long memberShipNumber) {
+        return clientRepository.findByMemberShipNumber(memberShipNumber)
+                .orElseThrow(NotFoundException::new);
+    }
+
+    /**
+     * Получения списка клиентов с пагинацией
+     *
+     * @param pageParams Параметры пагинации
+     * @return Список клиентов
+     */
+    public ClientInfoPageable getClientList(PageParams pageParams) {
+        val pageable = getPageRequest(pageParams);
+
+        val pageClient = clientRepository.findAll(pageable);
+
+        return clientMapper.createClientInfoPageable(pageClient, pageable);
+    }
+
+    /**
+     * Построение параметров пагинации
+     *
+     * @param pageParams Пагинация ДТО
+     * @return Пагинация БД
+     */
+    private PageRequest getPageRequest(PageParams pageParams) {
+
+        return PageRequest.of(
+                Optional.ofNullable(pageParams.getPageCurrent())
+                        .orElse(0),
+                Optional.ofNullable(pageParams.getPageSize())
+                        .orElse(appConfig.getDefaultPageSize()));
     }
 }
